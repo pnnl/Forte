037375f2a8469559ada401ad2d966c47f06f8e97
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2022-07-20 19:37:37 -0400

    task: added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import { Grid } from '@mui/material/Grid';
+import Grid from '@mui/material/Grid';
+import AppBar from './Components/appbar/AppBar';
 import * as d3 from "d3";
 //import logo from './logo.svg';
 import './App.css';
@@ -11,7 +12,7 @@ import './App.css';
 class App extends Component{
   constructor(props){
     super(props);
-    console.log()
+    console.log();
   }
 
   componentDidMount(){
@@ -25,7 +26,15 @@ class App extends Component{
   render(){
 
     return(
-      "Hello"
+      <Grid container spacing={0} >
+          <Grid item xs={12}><AppBar style={{height:"5vh"}}></AppBar></Grid>
+          {/* <Grid item xs={12}>
+          <Grid container style={{opacity:(this.props.isLoadingUpdate)?0.4:1}}>
+          <Grid item xs={12} lg={5}><CardLeft></CardLeft></Grid>
+          <Grid item xs={12} lg={7}><CardRight></CardRight></Grid>
+          </Grid>
+        </Grid> */}
+        </Grid>
     )
   }
 }
---
+++ src/Components/appbar/AppBar.js
@@ -0,0 +1,90 @@
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
+import { connect } from "react-redux";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+export class  SearchAppBar extends Component {
+  //const classes = useStyles();
+  constructor(props) {
+    super(props);
+    console.log();    
+}
+componentDidMount() {}
+componentDidUpdate() {}
+shouldComponentUpdate(nextProps, nextState){
+    return true
+}
+
+
+render(){ 
+//const { selected_list } = this.state;  
+var break_character = <div><br /></div>
+return (
+
+<Navbar bg="light" variant="light" sticky="top" expand="lg">
+  <Container style={{margin:0}}>
+    <Navbar.Brand ><b>kPF-AE-LSTM</b></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav  className="me-auto">
+        {/* <Nav.Item style={{opacity:(this.props.isLoadingUpdate)?0.4:1}}><GranularitySelectionInitial location={"appbar"}></GranularitySelectionInitial></Nav.Item> */}
+        <Nav.Item>Dummy</Nav.Item>
+        {/* <Nav.Item style={{opacity:(this.props.isLoadingUpdate)?1:1}}>
+        <UpdateButton></UpdateButton>
+        </Nav.Item> */}
+       
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    
+  );
+ } //return ends
+}
+
+const maptstateToprop = (state) => {
+  return {
+    blank_placeholder: state.blank_placeholder,
+    
+  }
+}
+const mapdispatchToprop = (dispatch) => {
+  return {
+      set_blank_placeholder: (val) => dispatch({ type: "blank_placeholder", value: val }),
+    
+  }
+}
+
+export default connect(maptstateToprop, mapdispatchToprop)(SearchAppBar);
+
+/* export default function SearchAppBar() {
+  //const classes = useStyles();
+  return (
+    
+
+<Navbar bg="light" variant="light" sticky="top" expand="lg">
+  <Container style={{margin:0}}>
+    <Navbar.Brand ><b>PRIVEE</b></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav  className="me-auto">
+        <Nav.Item>
+        Individual Dataset
+        </Nav.Item>
+        <Nav.Item>
+        Aggregated Dataset
+        </Nav.Item>
+        <Nav.Item>
+        <Nav.Link href="" >Aggregated Datasets</Nav.Link>
+        </Nav.Item>
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    
+  );
+} */

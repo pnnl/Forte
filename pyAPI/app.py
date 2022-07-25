fe9717d69a4a0b93c4b624e9f64cba642283d05b
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2022-07-25 16:54:32 -0400

    task: model is saved successfully

--- pyAPI/app.py
+++ pyAPI/app.py
@@ -6,13 +6,33 @@ from flask_cors import CORS
 import requests, ast
 import json
 import logging
-from itertools import combinations
+import tensorflow as tf
+import tensorflow_probability as tfp
 
 app = Flask(__name__)
 CORS(app)
 logging.basicConfig(filename='pyAPI/logs/flask.log',level=logging.DEBUG,format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s') #https://www.scalyr.com/blog/getting-started-quickly-with-flask-logging/
 path_parent = os.getcwd()
 
+# Non-callable functions
+def NLL(y, distr): 
+    sy = distr.mean()
+    return 1*-distr.log_prob(y)+tf.keras.losses.mean_squared_error(y, sy)
+
+my_model = tf.keras.models.load_model(path_parent+"/data/models/model_rnn_probab_nonsol.h5", custom_objects={'NLL': NLL})
+
+# Callable functions
+@app.route('/processor',methods = ['POST', 'GET'])
+def processor():
+    final_result2 ={"message": "This is not an error. This endpoint is not configured for public use."}
+    input = ""
+    model = ""
+    output = ""
+    print(my_model.summary())
+    
+    response=make_response(jsonify(final_result2), 200) #removed processing
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response;
 
 @app.route('/',methods = ['POST', 'GET'])
 def index():
